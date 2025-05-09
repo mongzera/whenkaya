608470c1d03555459959cde0a9bb67aa13abad7a
abedmasdoc <masdoc13.dm@gmail.com>
2025-05-09 13:13:47 +0800

    Update login.view.php

--- src/views/auth/login.view.php
+++ src/views/auth/login.view.php
@@ -5,12 +5,7 @@
             <input type="text" name="username" id="username" class="auth-input-field" placeholder="Username" required />
             <input type="password" name="password" id="password" class="auth-input-field" placeholder="Password" required />
             <input type="submit" value="Login Account" class="auth-input-field auth-submit" />
-            <p>Don't have an account? <a href="/create-account">Register</a></p>
-            <?php if (!empty($content["error"])): ?>
-                <div class="error-message">
-                    <?php echo htmlspecialchars($content["error"], ENT_QUOTES, 'UTF-8'); ?>
-                </div>
-            <?php endif; ?>
+            <p>Don't have an account? <a href="/create-account">Register</a></p>    
         </div>
     </form>
 </div>
