7621a8bd6c3143d06f4cab12d67a6d841eae2f1e
abedmasdoc <masdoc13.dm@gmail.com>
2025-05-09 13:36:58 +0800

    updated

--- src/views/auth/login.view.php
+++ src/views/auth/login.view.php
@@ -1,13 +1,22 @@
-<div class="auth-container ">
+<div class="auth-container">
     <form action="/login" method="POST">
         <p class='h2'>Login Account</p>
         <div class="auth-input-fields">
-            <input type="text" name="username" id="username" class="auth-input-field" placeholder="Username"/>
-            <input type="password" name="password" id="pasword" class="auth-input-field" placeholder="Password"/>
+            <input type="text" name="username" id="username" class="auth-input-field" placeholder="Username" value="<?= htmlspecialchars($content['old']['username'] ?? '') ?>" />
+            <?php if (!empty($content['errors']['username'])): ?>
+                <p class="error-message"><?= htmlspecialchars($content['errors']['username']) ?></p>
+            <?php endif; ?>
+
+            <input type="password" name="password" id="password" class="auth-input-field" placeholder="Password" />
+            <?php if (!empty($content['errors']['password'])): ?>
+                <p class="error-message"><?= htmlspecialchars($content['errors']['password']) ?></p>
+            <?php endif; ?>
+
             <input type="submit" value="Login Account" class="auth-input-field auth-submit" />
-            <?php
-                echo $content["error"];
-            ?> 
+            
+            <?php if (!empty($content['errors']['general'])): ?>
+                <p class="error-message"><?= htmlspecialchars($content['errors']['general']) ?></p>
+            <?php endif; ?>
         </div>
     </form>
 </div>
