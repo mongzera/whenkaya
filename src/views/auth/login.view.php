95cf1de5cb1d574032eff6cd013fa485b0d873dc
abedmasdoc <masdoc13.dm@gmail.com>
2025-05-09 12:56:27 +0800

    test

--- src/views/auth/login.view.php
+++ src/views/auth/login.view.php
@@ -1,16 +1,16 @@
-<div class="auth-container ">
+<div class="auth-container">
     <form action="/login" method="POST">
         <p class='h2'>Login Account</p>
         <div class="auth-input-fields">
-            <input type="text" name="username" id="username" class="auth-input-field" placeholder="Username"/>
-            <input type="password" name="password" id="password" class="auth-input-field" placeholder="Password"/>
+            <input type="text" name="username" id="username" class="auth-input-field" placeholder="Username" required />
+            <input type="password" name="password" id="password" class="auth-input-field" placeholder="Password" required />
             <input type="submit" value="Login Account" class="auth-input-field auth-submit" />
-            <p>Doesn't have an account? <a href="/create-account">Register</a></p>
-            <?php
-                if (isset($content["error"])) {
-                    echo $content["error"];
-                }
-            ?> 
+            <p>Don't have an account? <a href="/create-account">Register</a></p>
+            <?php if (!empty($content["error"])): ?>
+                <div class="error-message">
+                    <?php echo htmlspecialchars($content["error"], ENT_QUOTES, 'UTF-8'); ?>
+                </div>
+            <?php endif; ?>
         </div>
     </form>
 </div>
