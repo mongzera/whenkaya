74b27254a050150a4e4fd65d93d76bbab69c0e81
abedmasdoc <masdoc13.dm@gmail.com>
2025-05-09 13:50:28 +0800

    Update login.view.php

--- src/views/auth/login.view.php
+++ src/views/auth/login.view.php
@@ -1,22 +1,38 @@
 <div class="auth-container">
     <form action="/login" method="POST">
         <p class='h2'>Login Account</p>
+
+        <!-- General Error (e.g., Invalid Credentials) -->
+        <?php if (!empty($content['errors']['general'])): ?>
+            <div class="error-message">
+                <?= htmlspecialchars($content['errors']['general'], ENT_QUOTES, 'UTF-8') ?>
+            </div>
+        <?php endif; ?>
+
         <div class="auth-input-fields">
-            <input type="text" name="username" id="username" class="auth-input-field" placeholder="Username" value="<?= htmlspecialchars($content['old']['username'] ?? '') ?>" />
+            <!-- Username Input -->
+            <input type="text" name="username" id="username" class="auth-input-field" placeholder="Username" value="<?= htmlspecialchars($content['old']['username'] ?? '') ?>" required />
             <?php if (!empty($content['errors']['username'])): ?>
-                <p class="error-message"><?= htmlspecialchars($content['errors']['username']) ?></p>
+                <div class="error-message">
+                    <?= htmlspecialchars($content['errors']['username'], ENT_QUOTES, 'UTF-8') ?>
+                </div>
             <?php endif; ?>
 
-            <input type="password" name="password" id="password" class="auth-input-field" placeholder="Password" />
+            <!-- Password Input -->
+            <input type="password" name="password" id="password" class="auth-input-field" placeholder="Password" required />
             <?php if (!empty($content['errors']['password'])): ?>
-                <p class="error-message"><?= htmlspecialchars($content['errors']['password']) ?></p>
+                <div class="error-message">
+                    <?= htmlspecialchars($content['errors']['password'], ENT_QUOTES, 'UTF-8') ?>
+                </div>
             <?php endif; ?>
 
+            <!-- Submit Button -->
             <input type="submit" value="Login Account" class="auth-input-field auth-submit" />
-            
-            <?php if (!empty($content['errors']['general'])): ?>
-                <p class="error-message"><?= htmlspecialchars($content['errors']['general']) ?></p>
-            <?php endif; ?>
+        </div>
+
+        <!-- Lower Box for "Don't have an account?" -->
+        <div class="auth-lower-box">
+            <p>Don't have an account? <a href="/create-account">Register</a></p>
         </div>
     </form>
 </div>
