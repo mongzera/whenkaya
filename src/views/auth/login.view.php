471081844e6bf76665799f231923b65a0ae7c2d9
abedmasdoc <masdoc13.dm@gmail.com>
2025-05-09 13:27:20 +0800

    Update login.view.php

--- src/views/auth/login.view.php
+++ src/views/auth/login.view.php
@@ -1,11 +1,13 @@
-<div class="auth-container">
+<div class="auth-container ">
     <form action="/login" method="POST">
         <p class='h2'>Login Account</p>
         <div class="auth-input-fields">
-            <input type="text" name="username" id="username" class="auth-input-field" placeholder="Username" required />
-            <input type="password" name="password" id="password" class="auth-input-field" placeholder="Password" required />
+            <input type="text" name="username" id="username" class="auth-input-field" placeholder="Username"/>
+            <input type="password" name="password" id="pasword" class="auth-input-field" placeholder="Password"/>
             <input type="submit" value="Login Account" class="auth-input-field auth-submit" />
-            <p>Don't have an account? <a href="/create-account">Register</a></p>    
+            <?php
+                echo $content["error"];
+            ?> 
         </div>
     </form>
 </div>
