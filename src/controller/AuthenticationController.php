8a22640bb5efbad81cbb37cf332511821889978d
abedmasdoc <masdoc13.dm@gmail.com>
2025-05-16 04:16:56 +0800

    create account validation

--- src/controller/AuthenticationController.php
+++ src/controller/AuthenticationController.php
@@ -6,71 +6,104 @@ use Src\Db\Database;
 use Src\Middleware\Auth;
 use Src\Model\UserModel;
 
-class AuthenticationController extends BaseController{
+class AuthenticationController extends BaseController
+{
+    public function create_account()
+    {
+        if (Auth::user()) redirect("dashboard");
 
-    public function create_account(){
-        if(Auth::user()) redirect("dashboard");
+        $errors = [
+            'firstname' => '',
+            'lastname' => '',
+            'username' => '',
+            'email' => '',
+            'password' => '',
+            'retype_password' => ''
+        ];
 
-        //create new user logic
-        $error = "";
-        if(isPost()){
-
-
+        if (isPost()) {
             $firstname = cleanRequest($_POST['firstname']);
-            $lastname  = cleanRequest($_POST['lastname'] );
-            $username  = cleanRequest($_POST['username'] );
-            $password  = cleanRequest($_POST['password'] );
-            $email     = cleanRequest($_POST['email']    );
+            $lastname = cleanRequest($_POST['lastname']);
+            $username = cleanRequest($_POST['username']);
+            $password = cleanRequest($_POST['password']);
             $retype_password = cleanRequest($_POST['retype-password']);
+            $email = cleanRequest($_POST['email']);
 
-            
             $isAllInputValid = true;
-            
 
-            $hashed = password_hash($password, PASSWORD_DEFAULT);
-
-            if(!($password === $retype_password)) {
-                $error =  "<p style='color: red;'>Password mismatch!</p>";
+            // Validate inputs one by one and break on first error
+            if (empty($firstname)) {
+                $errors['firstname'] = "First name cannot be blank!";
+                $isAllInputValid = false;
+            } elseif (!preg_match('/^[a-zA-Z]+$/', $firstname)) {
+                $errors['firstname'] = "First name must only contain letters!";
                 $isAllInputValid = false;
             }
 
-            if($firstname == ''){
-                $error =  "<p style='color: red;'>First name cannot be blank!</p>";
+            if ($isAllInputValid && empty($lastname)) {
+                $errors['lastname'] = "Last name cannot be blank!";
+                $isAllInputValid = false;
+            } elseif ($isAllInputValid && !preg_match('/^[a-zA-Z]+$/', $lastname)) {
+                $errors['lastname'] = "Last name must only contain letters!";
                 $isAllInputValid = false;
             }
 
-            if($lastname == ''){
-                $error =  "<p style='color: red;'>Last Name cannot be blank!</p>";
+            if ($isAllInputValid && empty($username)) {
+                $errors['username'] = "Username cannot be blank!";
+                $isAllInputValid = false;
+            } elseif ($isAllInputValid && !preg_match('/^[a-zA-Z0-9._-]{3,30}$/', $username)) {
+                $errors['username'] = "Username must use letters, numbers,<br>dots, underscores, or hyphens.";
                 $isAllInputValid = false;
             }
 
-            if($isAllInputValid){
+            if ($isAllInputValid && empty($email)) {
+                $errors['email'] = "Email cannot be blank!";
+                $isAllInputValid = false;
+            } elseif ($isAllInputValid && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errors['email'] = "Invalid email format!";
+                $isAllInputValid = false;
+            }
+
+            if ($isAllInputValid && empty($password)) {
+                $errors['password'] = "Password cannot be blank!";
+                $isAllInputValid = false;
+            } elseif ($isAllInputValid && !preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/', $password)) {
+                $errors['password'] = "Password must be at least 8 characters<br>and include uppercase, lowercase,<br>number, and special character.";
+                $isAllInputValid = false;
+            }
+
+            if ($isAllInputValid && $password !== $retype_password) {
+                $errors['retype_password'] = "Password mismatch!";
+                $isAllInputValid = false;
+            }
+
+            // If all good, insert user
+            if ($isAllInputValid) {
+                $hashed = password_hash($password, PASSWORD_DEFAULT);
+
                 $user = new UserModel();
 
-                if($user->insert([
+                if ($user->insert([
                     'first_name' => $firstname,
                     'last_name' => $lastname,
                     'username' => $username,
                     'email' => $email,
                     'password_hashed' => $hashed
-                ])){
+                ])) {
                     Auth::authenticate_user($username, $password);
                     redirect("dashboard");
+                } else {
+                    $errors['general'] = "Account creation failed. Please try again.";
                 }
             }
-            
-            // if(db_create_user($username, $hashed)){
-            //     Auth::authenticate_user($username, $password);
-            //     redirect("dashboard");
-            
-            // }
         }
 
         $content = [
             "title" => "Create Account",
             "head" => "../src/views/default_head.php",
             "body" => "../src/views/auth/create-account.view.php",
-            "error" => $error
+            "errors" => $errors,
+            "old" => $_POST ?? [] // Pass old input values back to form
         ];
 
         $static = [
@@ -81,23 +114,46 @@ class AuthenticationController extends BaseController{
         render_page($content, $static);
     }
 
-    public function login_account(){
-        if(Auth::user()) redirect("dashboard");
+    public function login_account()
+    {
+        if (Auth::user()) redirect("dashboard");
 
-        //login user logic
-        if(isPost()){
+        $errors = [
+            'username' => '',
+            'password' => '',
+            'general' => ''
+        ];
+
+        if (isPost()) {
             $username = cleanRequest($_POST['username']);
             $password = cleanRequest($_POST['password']);
+            $isAllInputValid = true;
 
-            if(Auth::authenticate_user($username, $password)){
-                redirect("dashboard");
+            if (empty($username)) {
+                $errors['username'] = "Username cannot be blank!";
+                $isAllInputValid = false;
+            }
+
+            if ($isAllInputValid && empty($password)) {
+                $errors['password'] = "Password cannot be blank!";
+                $isAllInputValid = false;
+            }
+
+            if ($isAllInputValid) {
+                if (Auth::authenticate_user($username, $password)) {
+                    redirect("dashboard");
+                } else {
+                    $errors['general'] = "Invalid username or password!";
+                }
             }
         }
 
         $content = [
             "title" => "Login Account",
             "head" => "../src/views/default_head.php",
-            "body" => "../src/views/auth/login.view.php"
+            "body" => "../src/views/auth/login.view.php",
+            "errors" => $errors,
+            "old" => $_POST ?? []
         ];
 
         $static = [
@@ -108,7 +164,8 @@ class AuthenticationController extends BaseController{
         render_page($content, $static);
     }
 
-    public function logout(){
+    public function logout()
+    {
         Auth::logout();
         redirect("login_account_get");
     }
--- src/views/auth/create-account.view.php
+++ src/views/auth/create-account.view.php
@@ -1,20 +1,24 @@
 <div class="auth-container flex flex-col">
     <form action="/create-account" method="POST">
         <p class='h2'>Create Account</p>
+
+        <?php if (!empty($content['errors'])): ?>
+            <div class="error-messages" style="color: red; font-size: 0.8em; margin-bottom: 10px;">
+                <?php foreach ($content['errors'] as $error): ?>
+                    <p><?= $error ?></p>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
+
         <div class="auth-input-fields">
-            <input type="text" name="firstname" id="firstname" class="auth-input-field" placeholder="First Name"/>
-            <input type="text" name="lastname" id="lastname" class="auth-input-field" placeholder="Last Name"/>
-            <input type="text" name="username" id="username" class="auth-input-field" placeholder="Username"/>
-            <input type="text" name="email" id="email" class="auth-input-field" placeholder="Email"/>
-            <input type="password" name="password" id="password" class="auth-input-field" placeholder="Password"/>
-            <input type="password" name="retype-password" id="retype-password" class="auth-input-field" placeholder="Retype-Password"/>
-            <input type="submit" value="Create Account" class="auth-input-field auth-submit" />
+            <input type="text" name="firstname" id="firstname" class="auth-input-field" placeholder="First Name" value="<?= htmlspecialchars($_POST['firstname'] ?? '') ?>">
+            <input type="text" name="lastname" id="lastname" class="auth-input-field" placeholder="Last Name" value="<?= htmlspecialchars($_POST['lastname'] ?? '') ?>">
+            <input type="text" name="username" id="username" class="auth-input-field" placeholder="Username" value="<?= htmlspecialchars($_POST['username'] ?? '') ?>">
+            <input type="text" name="email" id="email" class="auth-input-field" placeholder="Email" value="<?= htmlspecialchars($_POST['email'] ?? '') ?>">
+            <input type="password" name="password" id="password" class="auth-input-field" placeholder="Password">
+            <input type="password" name="retype-password" id="retype-password" class="auth-input-field" placeholder="Retype Password">
+            <input type="submit" value="Create Account" class="auth-input-field auth-submit">
+            <p>Already have an account? <a href="/login">Login</a></p>
         </div>
     </form>
-
-    <?php 
-    echo "<br>";
-    echo $content['error'];
-    
-    ?>
 </div>
